8eb6416a06078f2cbc36bb37e270a4cf1a503054
waffles <waffles@arbitraryexecution.com>
2022-05-01 22:36:57 -0400

    Typo fixes

--- reentrancy/challenges/ReentrancyGuardBank/ReentrancyGuardBank.test.js
+++ reentrancy/challenges/ReentrancyGuardBank/ReentrancyGuardBank.test.js
@@ -53,7 +53,7 @@ describe('ChecksEffectsInteractionsBank', function () {
       await expect(thiefContract.steal({ value: depositAmount })).to.be.reverted;
 
       thiefEndingBalance = await provider.getBalance(thief.address);
-      // not checking exact amounts here due to gas costs offseting precise balances
+      // not checking exact amounts here due to gas costs offsetting precise balances
       expect(thiefEndingBalance.lte(thiefBeginningBalance)).to.be.true;
     });
   });
