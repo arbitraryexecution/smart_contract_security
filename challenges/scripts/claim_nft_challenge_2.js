1771f9bb1c7d84dd60127fbdd67fdc3f650fced9
waffles <waffles@arbitraryexecution.com>
2022-05-01 21:19:37 -0400

    Added solver script for evm challenge 2

---
+++ challenges/scripts/claim_nft_challenge_2.js
@@ -0,0 +1,36 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const flag = await hre.ethers.getContractAt("RedFlag", "0xe42a6fC1aC0c4CCa1ABaE1B174B7D020652BA259")
+  console.log("Red Flag address:", flag.address);
+
+  // XXX: Set the correct answer and value here!
+  var answer = 0;
+  var value = 0;
+
+  // Mint the NFT!
+  var tx = await flag.mint(answer, value);
+  await tx.wait();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
